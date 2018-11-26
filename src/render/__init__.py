0c492c6d03e2980ae7c99f9a4c75949cc75f8365
Peter Uhlenbruck <peuhlenb@gmail.com>
2018-11-25 20:34:50 -0800

    render card based on input

--- src/render/__init__.py
+++ src/render/__init__.py
@@ -1,5 +1,6 @@
 import pygame
 from render.card import CardRenderer
+from card import Card, Suite, Value
 
 _black = 0, 0, 0
 _screen = None
@@ -19,10 +20,9 @@ def init():
 
 
 def render_screen():
-
+    card_to_render = Card(suite=Suite.SPADES, value=Value.JACK)
     card_back_rect = _card_back_image.get_rect()
-
     _screen.fill(_black)
     _screen.blit(_card_back_image, card_back_rect)
-    _screen.blit(_cardRenderer.render_card(card), card_back_rect.topright)
+    _screen.blit(_cardRenderer.render_card(card_to_render), card_back_rect.topright)
     pygame.display.flip()
--- src/render/card.py
+++ src/render/card.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import Surface, transform
+from card import Value, Suite
 
 _icon_size = _icon_width, _icon_height = (15, 20)
 _icon_position = _icon_position_x, _icon_position_y = (10, 5)
@@ -8,19 +9,23 @@ _icon_position = _icon_position_x, _icon_position_y = (10, 5)
 class CardRenderer:
     def __init__(self, base_image):
         self._card_base = base_image
-        self._spade_image = transform.scale(
-            pygame.image.load("res/spade.png").convert_alpha(), _icon_size
-        )
-        self._heart_image = transform.scale(
-            pygame.image.load("res/heart.png").convert_alpha(), _icon_size
-        )
+        self._suite_icons = {
+            Suite.SPADES: transform.scale(
+                pygame.image.load("res/spade.png").convert_alpha(), _icon_size
+            ),
+            Suite.HEARTS: transform.scale(
+                pygame.image.load("res/heart.png").convert_alpha(), _icon_size
+            ),
+        }
         self._card_font = pygame.font.SysFont("Arial", 30)
         self._icon_vertical_spacing = -5
 
     def render_card(self, card):
         rendered_card = self._card_base.copy()
         card_rect = rendered_card.get_rect()
-        value_and_suite_icon = self.render_value_and_suite("2", "spades")
+        value_and_suite_icon = self.render_value_and_suite(
+            card_display_value(card), card.suite
+        )
         rendered_card.blit(value_and_suite_icon, _icon_position)
         rendered_card.blit(
             transform.rotate(value_and_suite_icon, 180),
@@ -40,10 +45,24 @@ class CardRenderer:
         suite_icon_offset = (required_surface_size[0] - _icon_width) / 2
         icon_surface = Surface(required_surface_size, pygame.SRCALPHA, 32)
         value_icon = self._card_font.render(value, True, (0, 0, 0))
-        suite_icon = self._spade_image
+        suite_icon = self._suite_icons[suite]
         icon_surface.blit(value_icon, (0, 0))
         icon_surface.blit(
             suite_icon,
             (suite_icon_offset, value_icon.get_height() + self._icon_vertical_spacing),
         )
         return icon_surface
+
+
+def card_display_value(card):
+    card_value = card.value
+    if card_value == Value.ACE:
+        return "A"
+    elif card_value == Value.KING:
+        return "K"
+    elif card_value == Value.QUEEN:
+        return "Q"
+    elif card_value == Value.JACK:
+        return "J"
+    else:
+        return str(card_value.value)
